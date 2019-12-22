ef85b1a1951ab00c1177b94506f9f00674f57d49
Ioanna <lampropoi@fireworks.home>
2019-12-22 22:47:08 +0000

    add comments for the fuctionality of calculateBasket function

--- src/calculateBasket.js
+++ src/calculateBasket.js
@@ -1,5 +1,16 @@
 const products = require("./products");
 
+/**
+ * This function calculates the total price of the valid items in a basket and also returns the items
+ * that could not be scanned. To optimise the algorithm so as to have time complexity O(N),
+ * the items are validated in the same for-loop in which the total price of the basket is calculated.
+ * In a real world application, it might be better to have one fuction that validates the input and one
+ * that calculates the total to follow the separation of concerns(SoC) design pattern
+ *
+ * @param string[] items items inside a basket
+ * @returns {total: Number, error: string} total amount of basket and errors in input
+ */
+
 module.exports = items => {
   let totalPrice = 0;
   const scannedItems = {};
